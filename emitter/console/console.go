ed0f93657f14d1faa6195fc21e913341dc88fd82
Shaba Abhiram <shabarivas.abhiram@gmail.com>
2018-04-03 16:51:52 -0700

    Implment history search when piped into app

--- emitter/console/console.go
+++ emitter/console/console.go
@@ -1,13 +1,16 @@
 package console
 
+////////////////////////////////////////////////////////////////////////////////
+
 import (
-	"fmt"
 	"os"
 	"text/template"
 
 	"github.com/sabhiram/hist/emitter"
 	"github.com/sabhiram/hist/types"
 )
+
+////////////////////////////////////////////////////////////////////////////////
 
 func consoleEmit(ll []*types.LineDesc) error {
 	t, err := template.New("console").Parse(`{{ range $i, $l := .}}
@@ -21,7 +24,8 @@ func consoleEmit(ll []*types.LineDesc) error {
 	return t.Execute(os.Stdout, ll)
 }
 
+////////////////////////////////////////////////////////////////////////////////
+
 func init() {
-	fmt.Printf("Registering the console emitter.\n")
 	emitter.RegisterEmitter("console", "console", "co", consoleEmit, false)
 }
--- main.go
+++ main.go
@@ -65,12 +65,6 @@ func main() {
 		return
 	}
 
-	// Read stdin.
-	// TODO:
-	// 1. If we find the specified tag, then we are done and we have the list
-	// 	  of items we need to emit.
-	// 2. If the tag is not set, we go until we find the last tag of the hist
-	//	  tool in the history buffer.
 	r := bufio.NewReader(os.Stdin)
 	lds := []*types.LineDesc{}
 	for i := 1; true; i++ {
@@ -78,10 +72,12 @@ func main() {
 		if err != nil && err == io.EOF {
 			break
 		}
+		// TODO: Message should be annotated by the user.
 		msg := fmt.Sprintf("Count = %d", i)
 		lds = append([]*types.LineDesc{types.NewLineDesc(l, msg)}, lds...)
 	}
 
+	tagv := ""
 	i := 0
 	for i = 0; i < len(lds); i++ {
 		h := lds[i]
@@ -95,39 +91,39 @@ func main() {
 		cmd := arr[1]
 		h.Line = strings.Join(arr[1:], " ")
 		if cmd == "hist" {
-			fmt.Printf("Trying cmd = %s\n", h.Line)
 			fi := strings.Index(h.Line, "-tag")
 			if fi < 0 {
 				fi = strings.Index(h.Line, "-t")
 			} else {
-				fi += 3
+				fi += 4
 			}
 			if fi < 0 {
-				fmt.Printf("No tag found, but hist command\n")
 				break
 			} else {
-				fi += 1
+				fi += 2
 			}
 			if h.Line[fi] == ' ' || h.Line[fi] == '=' {
 				fi += 1
 			}
 			items := strings.Split(h.Line[fi:], " ")
-			tv := items[0]
-			fmt.Printf("Found tag set: %s\n", tv)
+			tagv = strings.TrimSuffix(items[0], "\n")
+			if tagv == cli.tag {
+				break
+			} else if cli.tag == "" {
+				break
+			}
 		}
-
-		fmt.Printf("%d ==> %s\n", i, h.Line)
-		<-time.After(1 * time.Second)
+		<-time.After(100 * time.Millisecond)
 	}
 
-	// Trim
+	// Trim and reverse.
 	lds = lds[0:i]
+	ln := len(lds)
+	for i := 0; i < ln/2; i++ {
+		lds[i], lds[ln-i-1] = lds[ln-i-1], lds[i]
+	}
 
-	// If the "-<output>" option is specified, the selected lines are passed
-	// down to each appropriate <output> plugin.  For example:
-	//   "-go" will invoke the `go` plugin which emits go exec statements.
-	//   "-md" will invoke the `markdown` plugin to render markdown.
-	//   "-console" will dump commented console versions of the scripts.
+	// Emit any enabled emitters.
 	err = emitter.EmitEnabled(lds)
 	fatalOnError(err)
 }
